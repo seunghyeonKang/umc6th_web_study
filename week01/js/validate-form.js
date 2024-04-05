177e3e27cd80a19d4175eb693e540a133527a191
seunghyeonKang <number42isuser@gmail.com>
2024-04-06 01:41:40 +0900

    [0406] 회원가입 유효성 검사

--- week01/js/validate-form.js
+++ week01/js/validate-form.js
@@ -4,6 +4,7 @@ document
     var name = document.getElementById("name").value.trim();
     var email = document.getElementById("email").value.trim();
     var age = document.getElementById("age").value.trim();
+    // var age = parseInt(document.getElementById("age").value.trim());
     var password = document.getElementById("password").value.trim();
     var confirmPassword = document
       .getElementById("confirm-password")
@@ -17,6 +18,17 @@ document
       "confirm-password-error"
     );
 
+    var nameValid = document.getElementById("name-valid");
+    var emailValid = document.getElementById("email-valid");
+    var ageValid = document.getElementById("age-valid");
+    var passwordValid = document.getElementById("password-valid");
+    var confirmPasswordValid = document.getElementById(
+      "confirm-password-valid"
+    );
+
+    var passwordPattern =
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,12}$/;
+
     // 초기화
     nameError.textContent = "";
     emailError.textContent = "";
@@ -24,34 +36,78 @@ document
     passwordError.textContent = "";
     confirmPasswordError.textContent = "";
 
+    nameValid.textContent = "";
+    emailValid.textContent = "";
+    ageValid.textContent = "";
+    passwordValid.textContent = "";
+    confirmPasswordValid.textContent = "";
+
     // 유효성 검사
-    if (name === "") {
+    // 유효성 - 이름
+    if (name !== "") {
+      nameValid.textContent = "멋진 이름이네요!";
+    } else {
       nameError.textContent = "필수 입력 항목입니다!";
       event.preventDefault();
     }
-
-    if (email === "") {
+    // 유효성 - 이메일
+    if (email !== "" && email.includes("@")) {
+      emailValid.textContent = "올바른 이메일 형식입니다!";
+    } else {
       emailError.textContent = "올바른 이메일 형식이 아닙니다!";
       event.preventDefault();
     }
-
-    if (age === "") {
+    // 유효성 - 나이
+    if (
+      age !== "" &&
+      !isNaN(age) &&
+      age >= 19 &&
+      age.indexOf(".") === -1 &&
+      age.indexOf("-") === -1
+      //   && Number.isInteger(age)
+    ) {
+      ageValid.textContent = "올바른 나이 형식입니다!";
+    } else if (
+      age !== "" &&
+      !isNaN(age) &&
+      age <= 19 &&
+      age >= 0 &&
+      age.indexOf(".") === -1
+    ) {
+      ageError.textContent = "19세 이상만 가입 가능합니다.";
+      event.preventDefault();
+    } else {
       ageError.textContent = "나이를 입력해주세요!";
       event.preventDefault();
     }
-
-    if (password === "") {
-      passwordError.textContent = "비밀번호는 최소 4자리 이상이어야 합니다.";
+    // 유효성 - 비밀번호
+    if (password !== "" && passwordPattern.test(password)) {
+      passwordValid.textContent = "올바른 비밀번호 형식입니다!";
+    } else {
+      passwordError.textContent =
+        "비밀번호는 영어/숫자/특수문자 조합, 최소 4자 이상, 최대 12자리 이하여야 합니다.";
       event.preventDefault();
     }
-
+    // 유효성 - 비밀번호 확인
     if (confirmPassword === "") {
       confirmPasswordError.textContent = "필수 입력 항목입니다!";
       event.preventDefault();
-    }
-
-    if (password !== confirmPassword) {
+    } else if (password === confirmPassword) {
+      confirmPasswordValid.textContent = "비밀번호가 일치합니다.";
+    } else {
       confirmPasswordError.textContent = "비밀번호가 일치하지 않습니다!";
       event.preventDefault();
+    }
+
+    // 폼 제출 알림
+    if (
+      nameValid.textContent !== "" &&
+      emailValid.textContent !== "" &&
+      ageValid.textContent !== "" &&
+      passwordValid.textContent !== "" &&
+      confirmPasswordValid.textContent !== ""
+    ) {
+      event.preventDefault();
+      alert("umc 챌린저 가입을 축하합니다!!");
     }
   });
